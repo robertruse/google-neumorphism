b3c0bdb502ab5666bb24701857527c0155672601
Robert Ruse <robert.a.ruse@gmail.com>
2020-11-16 20:39:01 +0200

    Third commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+/* Search icon */
 const searchInput = document.querySelector("input[type=text]");
 const searchIcon = document.querySelector(".fa-search");
 
@@ -7,4 +8,27 @@ searchInput.addEventListener("blur", selectIcon);
 
 function selectIcon(e) {
   searchIcon.classList.toggle("blue-text");
+}
+
+/* Menu */
+const menuItem = document.querySelector("#menu");
+const menuButton = document.querySelector("#btn-menu");
+
+menuButton.addEventListener("click", selectMenu);
+
+function selectMenu(e) {
+  menuItem.classList.toggle("show");
+  this.classList.toggle("btn-up-on");
+}
+
+/* Dark mode */
+const htmlItem = document.querySelector("html");
+const buttonDark = document.querySelector("#btn-dark");
+
+buttonDark.addEventListener("click", selectButton);
+
+function selectButton(e) {
+  htmlItem.classList.toggle("dark-mode");
+  this.classList.toggle("btn-up-on");
+  //this.firstElementChild.classList.toggle("blue-text");
 }
